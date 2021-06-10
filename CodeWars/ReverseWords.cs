238e3356aec191a9f313dc1c3b990c03d1868579
Guilherme Ottoni Escarelli De Alencar <guiottonie@gmail.com>
2021-06-10 20:36:05 -0300

    add reverseWords kata

---
+++ CodeWars/ReverseWords.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    public static class ReverseWords
+    {
+        public static string Reverse(string str)
+        {
+            var strings = str.Split(' ');
+
+            string x = string.Empty;
+
+            for (int z = 0; z < strings.Length; z++)
+            {
+                for (int i = (strings[z].Length - 1); i >= 0; i--)
+                    x += strings[z][i];
+                x += " ";
+            }
+
+            return x.TrimEnd;
+        }
+    }
+}
